TouK/endpoints

Improve error messages reported by servers
Instead of just returning a Bad Request (400) response with no entity in case of client error, we now return a list of error messages. Fixes #307.

It also standardizes the behavior of the server interpreters: first, they tests whether an incoming request matches a given endpoint definition by checking whether its URL has the same number of segments as the endpoint and if the static segments of the endpoint have the same value in the request URL. Second, the request parameters (dynamic URL segments, query string parameters and header values) are decoded. Third, the request entity is decoded. These three steps are run sequentially: the request parameters are decoded only after the servers have checked that the request matches the endpoint definition, and the request entity is decoded only after the servers have successfully decoded the request parameters. But within a step, the validations are run independently of each others: a request that contains both an invalid path segment and an invalid query string parameter will produce two error messages.

As a consequence, authentication handling has to be done at the level of `Request[A]` instead of `RequestHeaders[A]` (the latter doesn’t anymore have the power of returning an HTTP response). In practice, this means that the `BasicAuthentication` trait now defines a custom `authenticatedRequest` constructor instead of a `basicAuthenticationHeader` constructor.

play/server-circe/src/main/scala/endpoints/play/server/circe/JsonEntities.scala
@@ -1,10 +1,10 @@
 package endpoints.play.server.circe
 
-import endpoints.algebra
+import endpoints.{Invalid, algebra}
 import endpoints.play.server.Endpoints
-import io.circe.{Json, parser, Decoder => CirceDecoder, Encoder => CirceEncoder}
-import play.api.mvc.Results
+import io.circe.{DecodingFailure, Json, ParsingFailure, parser, Decoder => CirceDecoder, Encoder => CirceEncoder}
 import Util.circeJsonWriteable
+import cats.Show
 import play.api.http.Writeable
 
 /**
@@ -24,9 +24,11 @@ trait JsonEntities extends Endpoints with algebra.JsonEntities {
 
   def jsonRequest[A : CirceDecoder]: RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { text =>
-      parser.parse(text)
-        .right.flatMap(CirceDecoder[A].decodeJson)
-        .left.map(ignoredError => Results.BadRequest)
+      parser.parse(text).left.map(Show[ParsingFailure].show)
+        .right.flatMap { json =>
+          CirceDecoder[A].decodeJson(json).left.map(Show[DecodingFailure].show)
+        }
+        .left.map(error => malformedRequest(Invalid(error)))
     }
 
   def jsonResponse[A : CirceEncoder]: ResponseEntity[A] =

endpoints4s/play

server-circe/src/main/scala/endpoints/play/server/circe/JsonEntities.scala
@@ -1,10 +1,10 @@
 package endpoints.play.server.circe
 
-import endpoints.algebra
+import endpoints.{Invalid, algebra}
 import endpoints.play.server.Endpoints
-import io.circe.{Json, parser, Decoder => CirceDecoder, Encoder => CirceEncoder}
-import play.api.mvc.Results
+import io.circe.{DecodingFailure, Json, ParsingFailure, parser, Decoder => CirceDecoder, Encoder => CirceEncoder}
 import Util.circeJsonWriteable
+import cats.Show
 import play.api.http.Writeable
 
 /**
@@ -24,9 +24,11 @@ trait JsonEntities extends Endpoints with algebra.JsonEntities {
 
   def jsonRequest[A : CirceDecoder]: RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { text =>
-      parser.parse(text)
-        .right.flatMap(CirceDecoder[A].decodeJson)
-        .left.map(ignoredError => Results.BadRequest)
+      parser.parse(text).left.map(Show[ParsingFailure].show)
+        .right.flatMap { json =>
+          CirceDecoder[A].decodeJson(json).left.map(Show[DecodingFailure].show)
+        }
+        .left.map(error => handleClientErrors(Invalid(error)))
     }
 
   def jsonResponse[A : CirceEncoder]: ResponseEntity[A] =